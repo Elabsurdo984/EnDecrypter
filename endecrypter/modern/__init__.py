74d162a5a7c30fb50dc5500a4d24996985df292a
Elabsurdo984 <matiassfernandez00@gmail.com>
2025-02-13 20:55:10 -0300

    1.12.6

--- endecrypter/modern/__init__.py
+++ endecrypter/modern/__init__.py
@@ -1,4 +1,5 @@
 from .des import DESCipher
 from .rc4 import RC4Cipher
+from .triple_des import TripleDESCipher
 
 __all__ = ['DESCipher', 'RC4Cipher']
--- setup.py
+++ setup.py
@@ -10,13 +10,8 @@ setup(
     author="Elabsurdo984",
     author_email="matiassfernandez00@gmail.com",
     description="A comprehensive encryption/decryption library",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Elabsurdo984/EnDecrypter.git",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
-        "Operating System :: OS Independent",
-    ],
     python_requires=">=3.12",
 )
